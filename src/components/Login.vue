398d920d30f71bf202a0260cffa257876b6fd9be
zhaoqinghui <995140339@qq.com>
2020-11-09 19:05:53 +0800

    引入数据库jpa,elemnet

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-      用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
-      <br><br>
-      密码:<input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
-      <br><br>
-      <button v-on:click="login">登录</button>
-    </div>
+  <body id="poster">
+  <el-form class="login-container" label-position="left" label-width="0px">
+    <h3 class="login-title">系统登录</h3>
+    <el-form-item>
+      <el-input type="test" v-model="loginForm.username" auto-complete="on" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input type="password" v-model="loginForm.password" auto-complete="on" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-form-item style="width: 100%">
+      <el-button class="login-button" type="primary" style="width: 100%;border:none;" v-on:click="login">登录</el-button>
+    </el-form-item>
+  </el-form>
+  </body>
+
 </template>
 
 <script>
@@ -14,8 +22,8 @@
          data(){
            return{
                loginForm:{
-               username:"",
-               password:""
+               username:"admin",
+               password:"123"
              },
              responseResult:{}
            }
@@ -38,5 +46,34 @@
 </script>
 
 <style scoped>
+  .login-container{
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin:90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #cac6c6;
+  }
+  .login-title{
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+  .login-button{
+    background: rgba(0, 0, 0, 0.72);
+  }
 
+
+  #poster{
+    background:url("../assets/background.jpg") no-repeat;
+    background-position: center;
+    height:100%;
+    width: 100%;
+    background-size:cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/css/global.css'
 
 //设置反向代理，前端请求默认发送到 http:localhost:8443/api
 var axios = require('axios')
@@ -11,9 +14,11 @@ axios.defaults.baseURL = 'http://localhost:8080/api'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
   components: { App },
   template: '<App/>'
